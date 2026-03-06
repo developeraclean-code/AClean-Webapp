d66eae2110c32aaca8d82d1027744fbab1ab259d
developeraclean-code <developer.aclean@gmail.com>
2026-03-06 21:09:37 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    chunkSizeWarningLimit: 2000,
-  },
-  server: {
-    port: 3000,
-  },
+  base: "./"
 })
